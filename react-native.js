30223e0ae75de42abfb5d0b1f92eaac848d99e27
Ktsierra <josejaviersierra1998@gmail.com>
2025-07-28 12:04:14 -0400

    fix: reverted to blank install to test

--- react-native.js
+++ react-native.js
@@ -1,25 +1,5 @@
 import { reactBaseConfig } from "./react.js";
-import * as reactNative from "eslint-plugin-react-native";
-import globals from "globals";
 
-/**
- * @param {string|string[]} projectPaths - One or more tsconfig paths (e.g., './tsconfig.app.json', './tsconfig.json')
- */
 export default function reactNativeConfig(projectPaths) {
-  return [
-    ...reactBaseConfig(projectPaths),
-    {
-      files: ["**/*.{ts,tsx,js,jsx}"],
-      plugins: {
-        "react-native": reactNative,
-      },
-      languageOptions: {
-        globals: {
-          ...globals.browser,
-          ...reactNative.environments["react-native"].globals,
-        },
-      },
-      rules: reactNative.configs.all.rules,
-    },
-  ];
+  return [...reactBaseConfig(projectPaths)];
 }
